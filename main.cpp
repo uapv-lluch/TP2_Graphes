382896da0a8b0e25d8171e5e7747bb2a95a97b8a
charly <oursgris84@gmail.com>
2019-02-25 22:29:00 +0100

    Initialisation de la matrice représentant le graphe et des matrices C, p et d

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 
 using namespace std;
 
-unsigned n = 6;
+unsigned n = 8;
 
-void MooreDijkstra() {
-    vector<unsigned> C = vector<unsigned>();
-    C.push_back(0);
+void MooreDijkstra(unsigned c) {
+    unsigned C[n];
+    C[0] = 0;
     unsigned d[n];
     unsigned pere[n];
     for (unsigned i = 0 ; i < n ; ++i) {
         pere[i] = 0;
+    }
+    for (unsigned j = 0 ; j < n ; ++j) {
+        d[j] = 99999;
     }
     unsigned j = 1;
     for (unsigned l = 1 ; l < n ; ++l) {
@@ -22,11 +25,25 @@ void MooreDijkstra() {
 
 int main() {
 
-    for (unsigned i = 0 ; i < n ; ++i) {
-        for (unsigned j = 0 ; j < n ; ++j) {
-//            c[i][j] = 0;
+    // Matrice représentative du graphe
+    unsigned c[n][n];
+    for (unsigned i=0; i < n; ++i){
+        for(unsigned j=0; j < n; ++j){
+            c[i][j] = 0;
         }
     }
-    MooreDijkstra();
+    c[0][1] = 2;
+    c[0][3] = 6;
+    c[1][4] = 7;
+    c[1][2] = 2;
+    c[2][5] = 2;
+    c[2][3] = 1;
+    c[3][6] = 4;
+    c[4][5] = 3;
+    c[4][7] = 3;
+    c[5][6] = 2;
+    c[6][7] = 2;
+
+    MooreDijkstra(c);
     return 0;
 }
